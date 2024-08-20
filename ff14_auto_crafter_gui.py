d8a6057861eb749d6f3d4849ee544f91a2e1cc79
doriller <harvest767@yahoo.co.jp>
2024-08-21 07:45:33 +0900

    表示の日本語化とマクロ2枚使用時のエラー不具合修正
    実行後のコマンドプロンプトに表示される言語が英語で分かりづらかったので日本語に変更。
    
    マクロ2枚使用時に実行できない不具合を修正

--- ff14_auto_crafter_gui.py
+++ ff14_auto_crafter_gui.py
@@ -10,7 +10,7 @@ import pyautogui
 # pyinstaller ff14_auto_crafter_gui.py --onefile
 
 def ff14_macro_one(shell, macro_key, decision_key, exec_cnt, sleep_cnt, exec_shutdown, left_key = None):
-    print('FF14 Crafter Macro One By GUI')
+    print('FF14 Crafter Macro One By Niku Kyuu')
     sleep(1)
     # Ff14 をアクティベート
     shell.AppActivate('FINAL FANTASY XIV')
@@ -30,12 +30,16 @@ def ff14_macro_one(shell, macro_key, decision_key, exec_cnt, sleep_cnt, exec_shu
         sleep(int(sleep_cnt))
 
         if cnt < 10:
-            print('***  ', cnt + 1, ' / ', exec_cnt, ' 回終了 ***')
+            echo_cnt = '/echo ***  ' + str(cnt + 1) + ' / ' + str(exec_cnt) + ' 回終了 ***'
+            #print('***  ', cnt + 1, ' / ', exec_cnt, ' 回終了 ***')
         else:
-            print('*** ', cnt + 1, ' / ', exec_cnt, ' 回終了 ***')
+            echo_cnt = '/echo *** ' + str(cnt + 1) + ' / ' + str(exec_cnt) + ' 回終了 ***'
+            #print('*** ', cnt + 1, ' / ', exec_cnt, ' 回終了 ***')
+        typewrite(shell, echo_cnt)
     # 制作終了
     key_down(shell, 'esc')
-    print('終了')
+    #print('終了')
+    typewrite(shell, '/echo 終了しました')
 
     # shutdown 処理
     if exec_shutdown == 'y':
@@ -46,12 +50,13 @@ def ff14_macro_one(shell, macro_key, decision_key, exec_cnt, sleep_cnt, exec_shu
         key_down(shell, decision_key)
 
 def ff14_macro_two(shell, first_key, second_key, decision_key, exec_cnt, sleep_cnt, exec_shutdown, left_key = None):
-    print('FF14 Crafter Macro Two By GUI')
+    print('FF14 Crafter Macro Two By Niku Kyuu')
     sleep(2)
     # Ff14 をアクティベート
     shell.AppActivate('FINAL FANTASY XIV')
     # 決定キーを押下
-    wshell.SendKeys(str(decision_key))
+    #wshell.SendKeys(str(decision_key))
+    key_down(shell, decision_key)
 
     for cnt in range(int(exec_cnt)):
         # 決定キーを押下
@@ -70,12 +75,16 @@ def ff14_macro_two(shell, first_key, second_key, decision_key, exec_cnt, sleep_c
         sleep(int(sleep_cnt))
 
         if cnt < 10:
-            print('***  ', cnt + 1, ' / ', exec_cnt, ' 回終了 ***')
+            echo_cnt = '/echo ***  ' + str(cnt + 1) + ' / ' + str(exec_cnt) + ' 回終了 ***'
+            #print('***  ', cnt + 1, ' / ', exec_cnt, ' 回終了 ***')
         else:
-            print('*** ', cnt + 1, ' / ', exec_cnt, ' 回終了 ***')
+            echo_cnt = '/echo *** ' + str(cnt + 1) + ' / ' + str(exec_cnt) + ' 回終了 ***'
+            #print('*** ', cnt + 1, ' / ', exec_cnt, ' 回終了 ***')
+        typewrite(shell, echo_cnt)
     # 制作終了
     key_down(shell, 'esc')
-    print('終了')
+    #print('終了')
+    typewrite(shell, '/echo 終了しました')
 
     # shutdown 処理
     if exec_shutdown == 'y':
@@ -102,27 +111,41 @@ def typewrite(shell, target_key):
 if __name__ == '__main__':
     # shellの作成
     shell = win32com.client.Dispatch('WScript.Shell')
-    macro_cnt = input('Enter How Many Macro Palette: ')
+    #macro_cnt = input('Enter How Many Macro Palette: ')
+    macro_cnt = input('使用するマクロパレットの数を入力: ')
     if int(macro_cnt) == 1:
-        macro_key = input('Enter Macro Key Button: ') # マクロ実行キー
-        decision_key = input('Enter Decision Key Button: ') # 決定キー
-        exec_cnt = input('Input Exec cnt: ') # マクロ実行回数
-        sleep_cnt = input('Input Time Diff: ') # マクロキー押下後のsleep時間(次の実行までのsleep)
-        exec_shutdown = input('If you want to SHUTDOWN after Craft? y or n: ') # クラフト終了後にシャットダウンするかどうか
+        #macro_key = input('Enter Macro Key Button: ') # マクロ実行キー
+        macro_key = input('実行するマクロのキーボードを入力: ') # マクロ実行キー
+        #decision_key = input('Enter Decision Key Button: ') # 決定キー
+        decision_key = input('決定キーのキーボードを入力: ') # 決定キー
+        #exec_cnt = input('Input Exec cnt: ') # マクロ実行回数
+        exec_cnt = input('マクロを実行する介入を入力: ') # マクロ実行回数
+        #sleep_cnt = input('Input Time Diff: ') # マクロキー押下後のsleep時間(次の実行までのsleep)
+        sleep_cnt = input('マクロ実行後の待機時間(秒)を入力: ') # マクロキー押下後のsleep時間(次の実行までのsleep)
+        #exec_shutdown = input('If you want to SHUTDOWN after Craft? y or n: ') # クラフト終了後にシャットダウンするかどうか
+        exec_shutdown = input('クラフト終了後にシャットダウンしますか? y or n: ') # クラフト終了後にシャットダウンするかどうか
         if exec_shutdown == 'y':
-            left_key = input('Enter Move Left Side Key: ')
+            #left_key = input('Enter Move Left Side Key: ')
+            left_key = input('カーソルを左へ移動するキーボードを入力: ')
             ff14_macro_one(shell, macro_key, decision_key, exec_cnt, sleep_cnt, exec_shutdown, left_key)
         else:
             ff14_macro_one(shell, macro_key, decision_key, exec_cnt, sleep_cnt, exec_shutdown)
     elif int(macro_cnt) == 2:
         first_key = input('Enter First Macro Key Button: ') # マクロ実行1枚目キー
+        first_key = input('1 回目に実行するマクロのキーボードを入力: ') # マクロ実行1枚目キー
         second_key = input('Enter Second Key Button: ') # マクロ実行2枚目キー
-        decision_key = input('Enter Decision Key Button: ') # 決定キー
-        exec_cnt = input('Input Exec cnt: ') # マクロ実行回数
-        sleep_cnt = input('Input Wait Time: ') # マクロキー押下後のsleep時間(次の実行までのsleep)
-        exec_shutdown = input('If you want to SHUTDOWN after Craft? y or n: ') # クラフト終了後にシャットダウンするかどうか
+        second_key = input('2 回目に実行するマクロのキーボードを入力: ') # マクロ実行2枚目キー
+        #decision_key = input('Enter Decision Key Button: ') # 決定キー
+        decision_key = input('決定キーのキーボードを入力: ') # 決定キー
+        #exec_cnt = input('Input Exec cnt: ') # マクロ実行回数
+        exec_cnt = input('マクロを実行する介入を入力: ') # マクロ実行回数
+        #sleep_cnt = input('Input Wait Time: ') # マクロキー押下後のsleep時間(次の実行までのsleep)
+        sleep_cnt = input('マクロ実行後の待機時間(秒)を入力: ') # マクロキー押下後のsleep時間(次の実行までのsleep)
+        #exec_shutdown = input('If you want to SHUTDOWN after Craft? y or n: ') # クラフト終了後にシャットダウンするかどうか
+        exec_shutdown = input('クラフト終了後にシャットダウンしますか? y or n: ') # クラフト終了後にシャットダウンするかどうか
         if exec_shutdown == 'y':
             left_key = input('Enter Move Left Side Key: ')
+            left_key = input('カーソルを左へ移動するキーボードを入力: ')
             ff14_macro_two(shell, first_key, second_key, decision_key, exec_cnt, sleep_cnt, exec_shutdown, left_key)
         else:
             ff14_macro_two(shell, first_key, second_key, decision_key, exec_cnt, sleep_cnt, exec_shutdown)
